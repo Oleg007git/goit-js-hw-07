0c2b477a0ef7870ffc1d650cec848fd7e3dbf710
Oleg007git <cusniroleg@icloud.com>
2024-03-02 12:44:58 -0800

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -7,9 +7,17 @@ function handleSubmit(event) {
 
   const elements = event.currentTarget.elements;
 
+  const emailValue = elements.email.value.trim();
+  const passwordValue = elements.password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('Заповніть обидва поля!!!');
+    return;
+  }
+
   const info = {
-    email: elements.email.value.trim(),
-    password: elements.password.value.trim(),
+    email: emailValue,
+    password: passwordValue,
   };
   console.log(info);
   event.currentTarget.reset();
