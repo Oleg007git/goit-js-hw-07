425b8279b44e9db6d8f02d9abaf2d6336d501fbe
Oleg007git <cusniroleg@icloud.com>
2024-03-01 18:41:20 -0800

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,4 @@
-const form = querySelector('.login-form');
+const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', handleSubmit);
 
@@ -8,11 +8,9 @@ function handleSubmit(event) {
   const elements = event.currentTarget.elements;
 
   const info = {
-    email: elements.email.value,
-    password: elements.password.value,
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
   };
+  console.log(info);
+  event.currentTarget.reset();
 }
-
-console.log(info);
-
-event.currentTarget.reset();
